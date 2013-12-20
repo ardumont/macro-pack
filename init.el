94091cac984ba283b9e17ae3a9eabee76e069678
Antoine R. Dumont <ext.adumont@kyriba.com>
2013-12-20 12:42:33 +0100

    Fix save-macro

--- init.el
+++ init.el
@@ -2,14 +2,16 @@
 (defun save-macro (name)
   "save a macro. Take a name as argument and save the last
      defined macro under this name at the end of your .emacs"
-     (interactive "SName of the macro :")  ; ask for the name of the macro
-     (kmacro-name-last-macro name)         ; use this name for the macro
-     (find-file init-file)            ; open ~/.emacs or other user init file
-     (goto-char (point-max))               ; go to the end of the .emacs
-     (newline)                             ; insert a newline
-     (insert-kbd-macro name)               ; copy the macro
-     (newline)                             ; insert a newline
-     (switch-to-buffer nil))               ; return to the initial buffer
+  (interactive "SName of the last macro to save: ")
+  (let ((macro-filename "~/.emacs-live-packs/dump-macro.el"))
+    (kmacro-name-last-macro name) ; use this name for the macro
+    (find-file macro-filename)
+    (goto-char (point-max))
+    (newline)
+    (insert-kbd-macro name)
+    (newline)
+    (save-buffer)
+    (switch-to-buffer nil)))
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
